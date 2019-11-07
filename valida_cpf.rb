fafac583af843ea8ab6a8c591d2af0cf8bc7c3b7
Eric Coutinho <ericcdsoouza@gmail.com>
2019-11-07 17:03:57 +0000

    att validador cpf

---
+++ valida_cpf.rb
@@ -0,0 +1,18 @@
+require "cpf_cnpj"
+
+def valida_cpf (cpf)
+    n_cpf = CPF.new(cpf)
+    n_cpf = n_cpf.formatted
+    v_cpf = CPF.valid?(cpf)
+
+    if (v_cpf == true)
+        puts "O cpf #{n_cpf} é válido!"
+    else
+        puts "O cpf #{n_cpf} é inválido!"
+    end
+end
+
+print "Qual o número do seu CPF? "
+    cpf = gets.chomp.to_i
+
+valida_cpf(cpf)
